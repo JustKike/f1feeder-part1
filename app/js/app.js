990e16383722d16c0b24026a596c954e651bf917
JustKike <jaircervantes11@gmail.com>
2022-09-29 23:26:57 -0700

    Correcciones en las rutas

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,24 @@
-angular.module('F1FeederApp', [
+'use strict';
+
+var F1FeederApp = angular.module('F1FeederApp', [
   'F1FeederApp.services',
   'F1FeederApp.controllers',
-  'ngRoute'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-	when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
-	when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
-	otherwise({redirectTo: '/drivers'});
+  'ngRoute',
+]);
+
+F1FeederApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when("/drivers", {
+      templateUrl: "partials/drivers.html", 
+      controller: "driversController"
+    })
+    .when("/drivers/:id", {
+      templateUrl: "partials/driver.html", 
+      controller: "driverController"
+    })
+    .otherwise({
+      redirectTo: '/drivers'
+    });
+
 }]);
+
